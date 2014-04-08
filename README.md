f4e6c45274d95ec3585b139ad4b9a7fd82bee5b1
youngsheep <yysheep1101@gmail.com>
2014-04-08 17:19:19 +0800

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+server:
+
+    npm install -d;
+    pomelo start;
+
+
+client:
+    
+    git submodule update --init --recursive
--- server/app.js
+++ server/app.js
@@ -1,34 +1,20 @@
 var pomelo = require('pomelo');
-var area = require('./app/models/area');
-var dataApi = require('./app/util/dataApi');
 
 /**
  * Init app for client.
  */
 var app = pomelo.createApp();
-app.set('name', 'treasures');
+app.set('name', 'hello');
 
-app.configure('production|development', 'gate', function(){
-  app.set('connectorConfig', {
-      connector : pomelo.connectors.hybridconnector
-  });
-});
-
+// app configuration
 app.configure('production|development', 'connector', function(){
-  app.set('connectorConfig', {
-      connector : pomelo.connectors.hybridconnector,
-      heartbeat : 100,
-      useDict : true,
-      useProtobuf : true
-  });
-});
-
-app.configure('production|development', 'area', function(){
-  var areaId = app.get('curServer').areaId;
-  if (!areaId || areaId < 0) {
-    throw new Error('load area config failed');
-  }
-  area.init(dataApi.area.findById(areaId));
+	app.set('connectorConfig',
+		{
+			connector : pomelo.connectors.hybridconnector,
+			heartbeat : 3,
+			useDict : true,
+			useProtobuf : true
+		});
 });
 
 // start app
