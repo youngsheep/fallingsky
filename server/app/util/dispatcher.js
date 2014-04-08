dd4bfc9515984be3745f102a1d76073ea296b555
youngsheep <yysheep1101@gmail.com>
2014-04-08 13:52:05 +0800

    remove

--- server/app/util/dispatcher.js
+++
@@ -1,7 +0,0 @@
-var crc = require('crc');
-
-module.exports.dispatch = function(uid, connectors) {
-	var index = Math.abs(parseInt(crc.crc32(uid)),16) % connectors.length;
-	return connectors[index];
-};
-
